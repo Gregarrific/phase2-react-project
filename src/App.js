092ea07ce8812134c1121f2615d0af5e992cdbdb
Gregory Dutton <greg.dutton@gmail.com>
2023-09-04 21:38:12 -0500

    Setup BrowserRouter, Routes and Route components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
 import Navbar from './Navbar';
 import RecipeList from './RecipeList';
+import RecipeBox from './RecipeBox';
 
 function App() {
   return (
     <div className="App">
           Phase 2 React Project
           <Navbar />
-          <RecipeList />
+          <Routes>
+            <Route path="/recipes" element={<RecipeList />} />
+            <Route path="/favorites" element={<RecipeBox />} />
+            <Route path="/grocery-list" element={<GroceryList />} />
+          </Routes>
     </div>
   );
 }
