6a964c0cda76007586d3610bf97f43c6f13ef9fa
Gregory Dutton <greg.dutton@gmail.com>
2023-09-09 12:18:14 -0500

    Add saveRecipe onClick functionality

--- src/RecipeBox.js
+++ src/RecipeBox.js
@@ -2,6 +2,14 @@ import Recipe from "./Recipe";
 import { Row, Col } from "react-bootstrap";
 
 function RecipeBox({recipes}) {
+
+    function saveRecipe(e) {
+        const data = e.target.id.split("-");
+        const recipeID = data[1];
+        const status = (data[0] === "T" ? true : false);
+        handleSave(recipeID, status);
+    }
+
     return (
         <div>
         <h1>Saved Recipes</h1>
@@ -10,7 +18,7 @@ function RecipeBox({recipes}) {
         .map( (recipe, index) => {
             return (
                 <Col md={'auto'} key={index}>
-                <Recipe key={recipe.id} recipe={recipe} border="danger" />
+                <Recipe key={recipe.id} recipe={recipe} border="danger" saveReceipe={saveRecipe} />
                 </Col>
             );
         })}
