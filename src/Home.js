4310b4aa6046d64fad21b1f2c1d0f41e436c8848
Gregory Dutton <greg.dutton@gmail.com>
2023-09-10 14:15:10 -0500

    Update Home component with useful information about application

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,31 @@
 function Home() {
     return (
-        <h1>Home</h1>
+        <div>
+        <h1>Welcome to the Recipe/Grocery Tracker</h1>
+        <p>With this application, you can easily:</p>
+        <ol>
+            <li>Search for recipes on the Recipe Finder tab.</li>
+                <ul>
+                    <li>Searchable recipe database.</li>
+                    <li>Review recipe directions and ingredients.</li>
+                    <li>Save recipes that you want to cook later.</li>
+                    <li>View the recipes original web page for more information.</li>
+                </ul>
+            <li>Review recipes you have saved in the Saved Recipes tab.</li>
+                <ul>
+                    <li>Review recipe cards of recipes that you have saved.</li>
+                    <li>Remove recipes from your saved list you no longer want.</li>
+                    <li>Add ingredients from recipes to your Grocery List.</li>
+                </ul>
+            <li>Update your grocery list on the Grocery List tab.</li>
+                <ul>
+                    <li>Manually add items to your Grocery List.</li>
+                    <li>Mark off ingredients that you have purchased.</li>
+                    <li>Delete items that you no longer need on your list.</li>
+                </ul>
+        </ol>
+        <p>This application is made with React and styled with React Bootstrap.  Recipe data is from <a href="https://frosch.cosy.sbg.ac.at/datasets/json/recipes">here</a>.</p>
+        </div>
     )
 }
 
