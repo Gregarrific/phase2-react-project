83ae3d2fc3d20edf6a8b8950b77aafa432ab693a
Gregory Dutton <greg.dutton@gmail.com>
2023-09-10 18:22:27 -0500

    Fix spelling and grammatical errors

--- src/Home.js
+++ src/Home.js
@@ -8,20 +8,20 @@ function Home() {
                 <ul>
                     <li>Searchable recipe database.</li>
                     <li>Review recipe directions and ingredients.</li>
-                    <li>Save recipes that you want to cook for later.</li>
-                    <li>View the recipes original web page for more information.</li>
+                    <li>Save recipes that you want to cook later.</li>
+                    <li>View the recipe's original web page for more information.</li>
                 </ul>
             <li>Review recipes that you have saved in the Saved Recipes tab.</li>
                 <ul>
-                    <li>Review recipe cards of the recipes that you have saved.</li>
-                    <li>Remove recipes from your saved list that you no longer want.</li>
-                    <li>Add ingredients from recipes to your Grocery List.</li>
+                    <li>Review saved recipe cards.</li>
+                    <li>Remove saved recipes cards.</li>
+                    <li>Add ingredients from saved recipes to your Grocery List.</li>
                 </ul>
             <li>Update/View your grocery list on the Grocery List tab.</li>
                 <ul>
                     <li>Manually add items to your Grocery List.</li>
                     <li>Mark off ingredients that you have purchased.</li>
-                    <li>Delete items that you no longer need on your list.</li>
+                    <li>Delete items no longer needed on your list.</li>
                 </ul>
         </ol>
         <p>This application is made with React and styled with React Bootstrap.  Recipe data is from <a href="https://frosch.cosy.sbg.ac.at/datasets/json/recipes">here</a>.</p>
