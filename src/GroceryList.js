2e301503ad994662ca7954060a7e71de95283e4e
Gregory Dutton <greg.dutton@gmail.com>
2023-09-09 21:38:03 -0500

    Add Card component to table list and add purchase buttons

--- src/GroceryList.js
+++ src/GroceryList.js
@@ -1,30 +1,35 @@
-import { Table, Button } from "react-bootstrap";
+import { Table, Button, Card } from "react-bootstrap";
 
 function GroceryList({ groceryList }) {
  return (
    <div className="grocery-list">
       <h1>Grocery List</h1>
-      <Table striped bordered hover>
-         <thead>
-            <tr>
-               <th>DONE</th>
-               <th>ITEM</th>
-               <th>DELETE</th>
-            </tr>
-         </thead>
-         <tbody>
-         {groceryList.map( item => {
-            return (
+      <Card style={{ width: '45rem' }} className="mb-2">
+         <Table striped bordered hover size="sm">
+            <thead>
                <tr>
-                  <td></td>
-                  <td>{item.item.toUpperCase()}</td>
-                  <td><Button variant="outline-danger" size="sm">X</Button></td>
+                  <th>PURCHASED?</th>
+                  <th>ITEM</th>
+                  <th>DELETE</th>
                </tr>
-            )
-         })
-         }
-         </tbody>
-      </Table>
+            </thead>
+            <tbody>
+            {groceryList.map( item => {
+               return (
+                  <tr>
+                     { item.complete ? 
+                        <td><Button variant="success" size="sm">Yes</Button></td> :
+                        <td><Button variant="outline-success" size="sm">No</Button></td>
+                     }
+                     <td className={item.complete ? "purchased" : "not-complete"}>{item.item.toUpperCase()}</td>
+                     <td><Button variant="outline-danger" size="sm">x</Button></td>
+                  </tr>
+               )
+            })
+            }
+            </tbody>
+         </Table>
+      </Card>
     </div>
  )
 }
