ed39e0ac4818988ea0e60d3d3da26850768db07c
Gregory Dutton <greg.dutton@gmail.com>
2023-09-10 08:58:43 -0500

    Add state to Navbar for navigation buttons

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,40 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 import { Button } from "react-bootstrap";
+import { useState } from "react";
 
 function Navbar() {
+    const [activeLink, setActiveLink] = useState(0);
+
+    function handleButton(e) {
+        setActiveLink(parseInt(e.target.id));
+    }
+
     return (
         <nav>
-            <NavLink exact to="/">
-                <Button>Home</Button>
+            <NavLink exact="true" to="/">
+                <Button id="0" onClick={handleButton}
+                variant={(activeLink === 0 ? "primary" : "outline-primary")}>
+                    Home
+                </Button>
             </NavLink>{' '}
             <NavLink to="/recipes">
-                <Button>Recipe Finder</Button>
+                <Button id="1" onClick={handleButton}
+                variant={(activeLink === 1 ? "primary" : "outline-primary")}>
+                    Recipe Finder
+                </Button>
             </NavLink>{' '}
-            <NavLink to="/saved-recipes"><Button>Saved Recipes</Button></NavLink>{' '}
-            <NavLink to="/grocery-list"><Button>Grocery List</Button></NavLink>{' '}
+            <NavLink to="/saved-recipes">
+                <Button id="2" onClick={handleButton}
+                variant={(activeLink === 2 ? "primary" : "outline-primary")}>
+                    Saved Recipes
+                </Button>
+            </NavLink>{' '}
+            <NavLink to="/grocery-list">
+                <Button id="3" onClick={handleButton}
+                variant={(activeLink === 3 ? "primary" : "outline-primary")}>
+                    Grocery List
+                </Button>
+            </NavLink>{' '} 
         </nav>
     );
 
